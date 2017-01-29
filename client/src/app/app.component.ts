ee477687fd6da27cca978efdc34cf4926ebaa103
wiranoid <wiranoid@gmail.com>
2017-01-29 14:59:15 +0300

    Typing logic

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  text = `Three Rings for the Elven-kings under the sky,
-          Seven for the Dwarf-lords in halls of stone,
-          Nine for Mortal Men, doomed to die,
-          One for the Dark Lord on his dark throne
-          In the Land of Mordor where the Shadows lie.
-          One Ring to rule them all, One Ring to find them,
-          One Ring to bring them all and in the darkness bind them.
-          In the Land of Mordor where the Shadows lie.`;
+    text = `One Ring to rule them all`;
+
+    onFinished(raceInfo: any) {
+        console.log('Race is finished!', raceInfo);
+    }
 }
--- client/src/app/typewriter/typewriter.component.ts
+++ client/src/app/typewriter/typewriter.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
 import {Subject} from "rxjs";
 
 @Component({
@@ -8,27 +8,89 @@ import {Subject} from "rxjs";
 })
 export class TypewriterComponent implements OnInit {
     @Input() text: string;
+    @Output() onFinished: EventEmitter<any> = new EventEmitter();
 
     words: Array<string>;
     currentWord: string = '';
     currentWordIndex: number = 0;
-
+    numberOfErrors: number = 0;
+    isCurrentWordIncorrect: boolean = false;
     input$ = new Subject();
 
     constructor() {
         this.input$
             .do(() => {
-                if (this.words[this.currentWordIndex].startsWith(this.currentWord) &&
-                    this.words[this.currentWordIndex].length === this.currentWord.length) {
-                    this.currentWord = '';
-                    this.currentWordIndex++;
+                this.isCurrentWordIncorrect = false;
+
+                if (this.hasError()) {
+                    this.handleError();
+                } else if (this.isWordFinished()) {
+                    this.goToTheNextWord();
+                } else if (this.isTypingFinished()) {
+                    this.goToTheNextWord();
+                    this.finish();
                 }
+
             })
             .subscribe();
     }
 
     ngOnInit() {
         this.words = this.text.replace(/\s+/g,' ').split(' ');
+    }
+
+    private hasError(): boolean {
+        let isErrorInLastWord = this.isLastWord() && !this.isCorrect();
+        let isErrorInOtherWords = this.isSpace() ?
+            !this.isTheSameLength(this.currentWord.slice(0, -1)) ||
+            ( this.isTheSameLength(this.currentWord.slice(0, -1)) &&
+                !this.isCorrect(this.currentWord.slice(0, -1)) ) :
+            !this.isCorrect();
+
+        return isErrorInLastWord || isErrorInOtherWords;
+    }
+
+    private isWordFinished(): boolean {
+        return this.isSpace() &&
+               this.isCorrect(this.currentWord.slice(0, -1)) &&
+               this.isTheSameLength(this.currentWord.slice(0, -1));
+    }
+
+    private isTypingFinished(): boolean {
+        return this.isLastWord() && this.words[this.currentWordIndex] === this.currentWord;
+    }
+
+    private isSpace(): boolean {
+        return this.currentWord.slice(-1) === ' ';
+    }
+
+    private isCorrect(word?: string): boolean {
+        return this.words[this.currentWordIndex].startsWith(word || this.currentWord);
+    }
+
+    private isLastWord(): boolean {
+        return this.words.length - 1 === this.currentWordIndex;
+    }
+
+    private isTheSameLength(word: string): boolean {
+        return this.words[this.currentWordIndex].length === word.length;
+    }
+
+    private handleError(): void {
+        this.isCurrentWordIncorrect = true;
+        this.numberOfErrors++;
+    }
+
+    private goToTheNextWord(): void {
+        this.currentWord = '';
+        this.currentWordIndex++;
+    }
+
+    private finish(): void {
+        this.input$.complete();
+        this.onFinished.emit({
+            numberOfErrors: this.numberOfErrors
+        });
     }
 
 }
