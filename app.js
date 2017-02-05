8d248935f11b3b2d384232923552c73671554f73
wiranoid <wiranoid@gmail.com>
2017-02-05 17:49:44 +0300

    Basic socket.io setup

--- app.js
+++ app.js
@@ -8,4 +8,24 @@ mongoose.connect('mongodb://localhost/spaceracersdb');
 app.use(require('./routes/api'));
 app.use(serve(__dirname + '/client/dist'));
 
-app.listen(3000);
+const server = require('http').createServer(app.callback());
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+    console.log('user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('add-message', message => {
+        io.emit('message', {
+            type: 'new-message',
+            text: message
+        });
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Listening on port 3000...');
+});
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import * as io from 'socket.io-client';
+import {Observable} from "rxjs";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
     text = `One Ring to rule them all`;
+
+    private url = 'http://localhost:3000';
+    private socket;
+    messages = [];
+    connection;
+    message;
+
+    ngOnInit() {
+        this.connection = this.getMessages().subscribe(message => {
+            this.messages.push(message);
+        })
+    }
+
+    ngOnDestroy() {
+        this.connection.unsubscribe();
+    }
+
+    sendMessage() {
+        this.socket.emit('add-message', this.message);
+        this.message = '';
+    }
+
+    getMessages() {
+        return new Observable(observer => {
+            this.socket = io(this.url);
+            this.socket.on('message', data => {
+                observer.next(data);
+            });
+            return () => {
+                this.socket.disconnect();
+            }
+        });
+    }
 
     onFinished(raceInfo: any) {
         console.log('Race is finished!', raceInfo);
