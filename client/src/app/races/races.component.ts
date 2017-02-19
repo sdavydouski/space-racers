3a96609117c293ee20e91fee5e840a3195bd971d
wiranoid <wiranoid@gmail.com>
2017-02-19 22:14:07 +0300

    Socket connection/disconnection

--- client/src/app/races/races.component.ts
+++ client/src/app/races/races.component.ts
@@ -1,13 +1,27 @@
 import {Component} from "@angular/core";
+import * as io from 'socket.io-client';
+import Socket = SocketIOClient.Socket;
 
 @Component({
     template: `<app-typewriter [text]="text" (onFinished)="onFinished($event)"></app-typewriter>`
 })
 export class RacesComponent {
     text: string;
+    url: string = location.origin;
+    socket: Socket;
 
     constructor() {
         this.text = 'It\'s the job that\'s never started as takes longest to finish.';
+
+        this.socket = io(this.url);
+
+        this.socket.on('connect', () => {
+            console.log('connected ', this.socket.id);
+        });
+
+        this.socket.on('disconnect', () => {
+            console.log('disconnected ', this.socket.id);
+        });
     }
 
     onFinished(raceInfo: any) {
--- socket/index.js
+++ socket/index.js
@@ -2,10 +2,11 @@ module.exports = server => {
     const io = require('socket.io').listen(server);
 
     io.on('connect', socket => {
-        console.log('Client connected ', + socket.id);
+        console.log('Client connected ' + socket.id);
+
+        socket.on('disconnect', () => {
+            console.log('Client disconnected ' + socket.id);
+        })
     });
 
-    io.on('disconnect', socket => {
-        console.log('Client disconnected ', + socket.id);
-    });
 };
